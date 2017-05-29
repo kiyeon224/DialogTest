cd6ae2ccccc965e490565dedb52f1877a34e2e66
kiyeon224 <kiyeon224@naver.com>
2017-05-29 18:57:34 +0900

    #3 Make Radio Button

--- app/src/main/java/kiyeon224/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/kiyeon224/kr/hs/emirim/dialogtest/MainActivity.java
@@ -24,14 +24,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("첫번째 다이얼로그");
         dialog.setIcon(R.mipmap.hearts);
-//        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
-        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+//      dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+//      단순 목록
+/*      dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+             butDialog.setText(itemArr[i]);
+          }
+        });*/
+
+//      라디오 버튼 목록
+        dialog.setSingleChoiceItems(itemArr, 0, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onClick(DialogInterface dialog, int i) {
                 butDialog.setText(itemArr[i]);
             }
         });
-        dialog.setPositiveButton("OK", null);
+//      dialog.setPositiveButton("OK", null);
         dialog.show();
     }
 }
